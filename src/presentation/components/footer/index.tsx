0f29f425c67760b04ba6cb7b524a6847dae38ce5
victorguivasco <victorguilhermecavalcante@gmail.com>
2024-01-15 20:06:05 -0400

    feat: add footer

---
+++ src/presentation/components/footer/index.tsx
@@ -0,0 +1,39 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { InstagramLogo, WhatsappLogo } from '@phosphor-icons/react'
+
+import logoMini from '../../assets/logotipo.svg'
+
+function FooterComponent() {
+  return (
+    <div className="flex justify-center bg-[#272727]">
+      <div className="h-full w-full flex flex-col items-center max-w-7xl p-6 lg:py-8">
+        <div className="flex lg:flex-1">
+          <Link href="#">
+            <Image className="w-auto h-[4rem]" src={logoMini} alt="" />
+          </Link>
+        </div>
+
+        <div className="w-full h-[0px] mt-8 mb-6 border border-zinc-700" />
+
+        <div className="flex mb-6 gap-4 lg:flex-1">
+          <Link href="#">
+            <WhatsappLogo size={32} color="white" />
+          </Link>
+          <Link href="#">
+            <InstagramLogo size={32} color="white" />
+          </Link>
+        </div>
+
+        <p className="opacity-50 text-center text-white text-[9px] font-normal font-['Roboto'] tracking-tight">
+          Copyright © 2024 | All Rights Reserved
+          <br />
+          Created with love by Victor Guilherme
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export { FooterComponent }
--- src/presentation/pages/home/index.tsx
+++ src/presentation/pages/home/index.tsx
@@ -3,9 +3,9 @@ import { AboutPanel } from './components/aboutPanel'
 
 import { NavBarComponent } from '@/presentation/components/navBar'
 
-import styles from './styles.module.scss'
 import { MissionPanel } from './components/missionPanel'
 import { VocareCampPanel } from './components/vocarecampPanel'
+import { FooterComponent } from '@/presentation/components/footer'
 
 function HomePage() {
   return (
@@ -15,6 +15,7 @@ function HomePage() {
       <AboutPanel />
       <MissionPanel />
       <VocareCampPanel />
+      <FooterComponent />
     </div>
   )
 }
