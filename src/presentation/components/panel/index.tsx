038069711b0c7986a8de5dd63c6d668641f535a3
victorguivasco <victorguilhermecavalcante@gmail.com>
2024-01-19 22:45:38 -0400

    fix: fix color and image size

--- src/presentation/components/panel/index.tsx
+++ src/presentation/components/panel/index.tsx
@@ -1,16 +1,12 @@
 import { ReactNode } from 'react'
 
 interface PanelsProps {
-  backgroundColor?: string
   children: ReactNode
 }
 
-function Panel({ backgroundColor, children }: Readonly<PanelsProps>) {
-  const bgPanel = `bg-[${backgroundColor}]`
-  let className = 'flex justify-center ' + bgPanel
-
+function Panel({ children }: Readonly<PanelsProps>) {
   return (
-    <div className={className}>
+    <div className="flex justify-center">
       <div className="h-full w-full flex items-center max-w-7xl px-6 py-14 lg:py-12">
         <div className="w-full flex flex-col justify-center items-center gap-6 mx-auto lg:mx-0 lg:flex-row lg:max-w-7xl">
           {children}
@@ -20,12 +16,9 @@ function Panel({ backgroundColor, children }: Readonly<PanelsProps>) {
   )
 }
 
-function PanelVertical({ backgroundColor, children }: Readonly<PanelsProps>) {
-  const bgPanel = `bg-[${backgroundColor}]`
-  let className = 'flex justify-center ' + bgPanel
-
+function PanelVertical({ children }: Readonly<PanelsProps>) {
   return (
-    <div className={className}>
+    <div className="flex justify-center">
       <div className="h-full w-full flex items-center max-w-7xl px-6 py-14 lg:py-12">
         <div className="w-full flex flex-col justify-center items-center gap-6 mx-auto lg:mx-0 lg:max-w-7xl">
           {children}
